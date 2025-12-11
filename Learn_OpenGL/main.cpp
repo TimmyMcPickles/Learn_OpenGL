5b2ec9a56a44c08d7176e8fe128723ba2a69d84a
Ethan Prescott <kingscout1011@gmail.com>
2025-12-10 23:52:59 -0600

    Initial Commit

---
+++ Learn_OpenGL/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Learn_OpenGL
+//
+//  Created by Ethan Prescott on 12/10/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
