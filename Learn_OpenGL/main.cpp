2849d63061d762efe1e15d0ce8818ffef0265f2d
Ethan Prescott <kingscout1011@gmail.com>
2025-12-12 22:33:05 -0600

    indices

--- Learn_OpenGL/main.cpp
+++ Learn_OpenGL/main.cpp
@@ -90,6 +90,11 @@ int main(){
          0.0f,  0.5f, 0.0f
     };
     
+    unsigned int indices[] = {
+        0, 1, 3,
+        1, 2, 3
+    };
+    
     unsigned int VBO;       // Create vertex buffer objects
     unsigned int VAO;
     
