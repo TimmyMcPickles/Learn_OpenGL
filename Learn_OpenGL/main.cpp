3bad6eb47a94fa8e0db5c1d9c9902fc6e363813a
Ethan Prescott <kingscout1011@gmail.com>
2025-12-15 12:24:39 -0600

    Vertices

--- Learn_OpenGL/main.cpp
+++ Learn_OpenGL/main.cpp
@@ -85,9 +85,10 @@ int main(){
     glDeleteShader(fragmentShader);
     
     float vertices[] = {
-        -0.5f, -0.5f, 0.0f,
-         0.5f, -0.5f, 0.0f,
-         0.0f,  0.5f, 0.0f
+        0.5f, 0.5f, 0.0f,
+        0.5f,-0.5f, 0.0f,
+       -0.5f,-0.5f, 0.0f,
+       -0.5f, 0.5f, 0.0f
     };
     
     unsigned int indices[] = {
@@ -97,14 +98,19 @@ int main(){
     
     unsigned int VBO;       // Create vertex buffer objects
     unsigned int VAO;
+    unsigned int EBO;
     
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);  // Generate said buffer
+    glGenBuffers(1, &EBO);
     
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO); // Bind VBO to array buffer
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
     
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
